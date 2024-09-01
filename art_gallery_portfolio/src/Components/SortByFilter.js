59506fa90126f45ff26c9c672cd4415d000285bc
SebC750 <SebC89999@gmail.com>
2024-09-01 02:58:27 -0400

    initialized sorting functions

---
+++ art_gallery_portfolio/src/Components/SortByFilter.js
@@ -0,0 +1,14 @@
+
+function selectionSort(list, filterType){
+      
+}
+export function sortByFileSize(list){
+       selectionSort(list, 'file')
+}
+export function sortByTitle(list){
+    selectionSort(list, 'file')
+}
+export function sortByYear(list){
+    selectionSort(list, 'file')
+}
+
