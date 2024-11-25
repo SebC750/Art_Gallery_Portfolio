4c8faa8ada97b3c200a6e3a2d4ea57184d17706a
SebC750 <SebC89999@gmail.com>
2024-11-25 14:23:28 -0500

    Node.js Backend init - Initialized Node.js backend structure. - Defined models for artworks, users and comments to provide a blueprint for the object structure. - Initialized CRUD routes for retrieving and displaying artworks artwork, user and comment controllers.

---
+++ art_gallery_backend/models/comment.js
@@ -0,0 +1,29 @@
+class Comment{
+    Comment(userId, artworkId, content, likes, dislikes,  datePosted){
+             this.userId = userId;
+             this.artworkId = artworkId;
+             this.content = content;
+             this.likes = likes;
+             this.dislikes = dislikes;
+             this.datePosted = datePosted
+    }
+    getAllCommentsByUserId(userId){
+                  
+    }
+    getCommentByUserId(userId){
+
+    }
+    getAllCommentsByArtworkId(artworkId){
+
+    }
+    postNewCommentByUserId(userId, artworkId){
+
+    }
+    updateCommentByUserId(userId, artworkId){
+
+    }
+    deleteCommentByUserId(userId, artworkId){
+
+    }
+
+}
